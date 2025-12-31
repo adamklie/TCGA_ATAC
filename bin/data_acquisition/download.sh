eac597ce91652005cd22be2b812b67007275db74
adamklie <aklie@ucsd.edu>
2025-12-31 12:40:27 -0800

    added bin and metadata

---
+++ bin/data_acquisition/download.sh
@@ -0,0 +1,3 @@
+#wget https://gdc.cancer.gov/node/1060/ -O /cellar/users/aklie/data/datasets/TCGA_ATAC/metadata/2024_02_15/open_access.tsv
+#wget https://gdc.cancer.gov/node/1061/ -O /cellar/users/aklie/data/datasets/TCGA_ATAC/metadata/2024_02_15/controlled_access.tsv
+
